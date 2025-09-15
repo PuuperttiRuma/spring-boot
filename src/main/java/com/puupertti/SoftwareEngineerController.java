83180ec431c8614de89f8d91f236381075d2c3b3
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2025-09-15 15:10:03 +0300

    Add dummy API

---
+++ src/main/java/com/puupertti/SoftwareEngineerController.java
@@ -0,0 +1,20 @@
+package com.puupertti;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return List.of(
+            new SoftwareEngineer(1, "Jeff", "js, node, react, tailwind"),
+            new SoftwareEngineer(2, "Anna", "java, spring boot")
+        );
+    }
+}
