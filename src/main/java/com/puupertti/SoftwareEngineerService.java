037c5152513d93ce19442d55ab68b2e44e081bbc
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2025-09-16 15:29:30 +0300

    Add POST to create new sw engineer

--- src/main/java/com/puupertti/SoftwareEngineerService.java
+++ src/main/java/com/puupertti/SoftwareEngineerService.java
@@ -15,4 +15,8 @@ public class SoftwareEngineerService {
     public List<SoftwareEngineer> getAllSoftwareEngineers(){
         return softwareEngineerRepository.findAll();
     }
+
+    public void insertSoftwareEngineer(SoftwareEngineer softwareEngineer) {
+        softwareEngineerRepository.save(softwareEngineer);
+    }
 }
